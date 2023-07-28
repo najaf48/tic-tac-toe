856fc680359095d42eab43a56211f2d2d3990708
Najaf <najaf1232075@gmail.com>
2023-07-29 00:31:44 +0530

    left to add rules for winning

--- board.py
+++ board.py
@@ -7,24 +7,43 @@ class Board:
                       ['_','_','_',]]
         self.font = pygame.font.SysFont(None, 50,bold=False)
         self.cords = [[50,50],[250,50],[450,50],[50,250],[250,250],[450,250],[50,450],[250,450],[450,450]]
+        self.cords1 = [[[50,50],[250,50],[450,50]],
+                       [[50,250],[250,250],[450,250]],
+                       [[50,450],[250,450],[450,450]]]
+
+    
     def draw_board(self,color:tuple):
+        # self.screen.fill((0,0,0))
         pygame.draw.line(self.screen,color,[250,50],[250,650],3)
         pygame.draw.line(self.screen,color,[450,50],[450,650],3)
         pygame.draw.line(self.screen,color,[50,250],[650,250],3)
         pygame.draw.line(self.screen,color,[50,450],[650,450],3)
     
     def draw_ring(self,position:list,color):
-        pygame.draw.circle(self.screen,color,position,40)
-        pygame.draw.circle(self.screen,(0,0,0),position,35)
+        pygame.draw.circle(self.screen,color,position,60)
+        pygame.draw.circle(self.screen,(0,0,0),position,55)
     
-    def draw_cross(self,dig1:list,dig2:list,color):
-        pygame.draw.line(self.screen,color,dig1[0],dig1[1],5)
-        pygame.draw.line(self.screen,color,dig2[0],dig2[1],5)
+    def draw_cross(self,line1:list,line2:list,color:tuple):
+        pygame.draw.line(self.screen,color,line1[0],line1[1],5)
+        pygame.draw.line(self.screen,color,line2[0],line2[1],5)
     
     def draw_text(self,text:str,color:tuple):
         img = self.font.render(text,True,color)
         self.screen.blit(img,(5,5))
     
+    def is_occupied(self,cord):
+        i = int((cord[0]-50)/200)
+        j = int((cord[1]-50)/200)
+        if self.board_representation[j][i]=='_':
+            return True
+        else:
+            return False
+    def edit_representation(self,cord,symbol):
+        for i in range(3):
+            for j in range(3):
+                if cord == [(200*i)+50,(200*j)+50]:
+                    self.board_representation[j][i]=symbol
+
     def which_cell(self,x,y):
         for cord in self.cords:
             if (x>cord[0] and x<(cord[0]+200)) and (y>cord[1] and y<(cord[1]+200)):
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from board import Board
 class tic_tac_toe:
     def __init__(self) -> None:
         pygame.init()
-        self.player_X=False
+        self.player_X=True
         self.setting = Settings()
         self.color = self.setting.color
         self.screen = pygame.display.set_mode((self.setting.screen_width,self.setting.screen_height))
         self.screen.fill(self.setting.bgcolor)
         self.board = Board(self.screen)
+        self.board.draw_board(self.setting.color)
+        self.board.draw_text("Player X chance",self.setting.color)
     
     def run(self):
         while True:
@@ -23,17 +25,32 @@ class tic_tac_toe:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     position=self.board.which_cell(event.pos[0],event.pos[1])
-                    print(event.pos)
-                    print(position)
-                    if self.player_X:
-                        ...
-                    else:
+                    is_occupied = self.board.is_occupied(position)
+                    if self.player_X and self.board.is_occupied(position):
+                        
+                        pygame.draw.rect(self.screen,self.setting.bgcolor,(0,0,400,50))
+                        self.board.draw_text("Player O chance",self.setting.color)
+                        
+                        line1 = [[position[0]+40,position[1]+40],[position[0]+160,position[1]+160]]
+                        line2 = [[position[0]+160,position[1]+40],[position[0]+40,position[1]+160]]
+                        
+                        self.board.draw_cross(line1,line2,self.setting.color)
+                        
+                        self.board.edit_representation(position,'x')
+                        self.player_X = False
+
+                    elif (not self.player_X) and self.board.is_occupied(position):
+                        
+                        pygame.draw.rect(self.screen,self.setting.bgcolor,(0,0,400,50))
+                        self.board.draw_text("Player X chance",self.setting.color)
+                        
                         pos = [position[0]+100,position[1]+100]
                         self.board.draw_ring(pos,self.setting.color)
-    
+                        
+                        self.board.edit_representation(position,'o')
+                        self.player_X = True
+
     def _update_screen(self):
-        self.board.draw_board(self.setting.color)
-        self.board.draw_text("Player X chance",self.setting.color)
         pygame.display.flip()
 
 if __name__=="__main__":
