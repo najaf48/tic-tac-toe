c956090f2c5e74f88d1b452d2b9327e713ff3174
Najaf <najaf1232075@gmail.com>
2023-07-28 16:50:21 +0530

    implemented till ring drawing

--- board.py
+++ board.py
@@ -5,17 +5,28 @@ class Board:
         self.board_representation = [['_','_','_',],
                       ['_','_','_',],
                       ['_','_','_',]]
-        self.cords = {[50,50]:0,[250,50]:1,[450,50]:2,[50,250]:3,[250,250]:4,[450,250]:5,[50,450]:6,[250,450]:7,[450,450]:8}
-        
-    def draw_ring(self,position):
-        pygame.draw.circle(self.screen,self.color,position,40)
+        self.font = pygame.font.SysFont(None, 50,bold=False)
+        self.cords = [[50,50],[250,50],[450,50],[50,250],[250,250],[450,250],[50,450],[250,450],[450,450]]
+    def draw_board(self,color:tuple):
+        pygame.draw.line(self.screen,color,[250,50],[250,650],3)
+        pygame.draw.line(self.screen,color,[450,50],[450,650],3)
+        pygame.draw.line(self.screen,color,[50,250],[650,250],3)
+        pygame.draw.line(self.screen,color,[50,450],[650,450],3)
+    
+    def draw_ring(self,position:list,color):
+        pygame.draw.circle(self.screen,color,position,40)
         pygame.draw.circle(self.screen,(0,0,0),position,35)
     
-    def draw_cross(self,dig1:list,dig2:list):
-        pygame.draw.line(self.screen,self.color,dig1[0],dig1[1],5)
-        pygame.draw.line(self.screen,self.color,dig2[0],dig2[1],5)
+    def draw_cross(self,dig1:list,dig2:list,color):
+        pygame.draw.line(self.screen,color,dig1[0],dig1[1],5)
+        pygame.draw.line(self.screen,color,dig2[0],dig2[1],5)
     
-    def draw_text(self,text):
-        img = self.font.render(text,True,self.color)
+    def draw_text(self,text:str,color:tuple):
+        img = self.font.render(text,True,color)
         self.screen.blit(img,(5,5))
+    
+    def which_cell(self,x,y):
+        for cord in self.cords:
+            if (x>cord[0] and x<(cord[0]+200)) and (y>cord[1] and y<(cord[1]+200)):
+                return cord
     
